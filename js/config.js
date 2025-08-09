7396f5b5dcbf040e0ec5ef25b769a44001aa9fc3
Umith Indula Wimalarathna <umithindula213@gmail.com>
2025-08-10 00:13:55 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_dXCEgYdoz', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '455a94vntpl7et5uqt003d5njt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_i9kMk9gJZ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '36b923fqppf4mcfpael1l02067', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://34929pmn98.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 

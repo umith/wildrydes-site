39cece8cecb724482848bb5ef3dee38e9c7c8805
Umith Indula Wimalarathna <umithindula213@gmail.com>
2025-08-09 19:15:09 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_lXQKdLIQx', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '35rf82k1b5qo54m6bt4d29igq8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '6a8dqhg01830f0u5j7cjhap7c3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
